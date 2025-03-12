57090f267025d5d1efb9ce35cb2c1f59e75829d8
sirezo <joebertcerezo@gmail.com>
2025-03-12 16:50:23 +0800

    Add user create domain

---
+++ app/models/domain/UserCreate.scala
@@ -0,0 +1,23 @@
+package models
+package domain
+
+import play.api.libs.json.*
+
+case class UserCreate(
+    name: String,
+    email: String,
+    username: String,
+    password: String,
+    avatar: Option[String]
+) {
+  def toDomain(): User = User(name, email, username, password)
+}
+
+object UserCreate {
+  given Format[UserCreate] = Json.format[UserCreate]
+  def unapply(
+      u: UserCreate
+  ): Option[(String, String, String, String, Option[String])] = Some(
+    Tuple.fromProductTyped(u)
+  )
+}
