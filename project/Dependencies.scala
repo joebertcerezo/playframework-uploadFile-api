6527ed305a6a89c3adf63f9214e39706bc34b16d
sirezo <joebertcerezo@gmail.com>
2025-03-12 16:49:25 +0800

    Edit dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 object Dependencies {
   val pekko      = "1.2.0-M1"
   val playMailer = "11.0.0-M1"
-  val slick      = "7.0.0-M1" // "6.1.1"
+  val slick      = "6.1.1"  // "6.1.1"
   val postgres   = "42.7.5" // "42.7.4"
   val catsCore   = "2.12.0"
   val jbcrypt    = "0.4"
