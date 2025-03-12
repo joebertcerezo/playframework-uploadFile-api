4e68b15f2dbbcbc43f76d7a7430ed531b0bf2c82
sirezo <joebertcerezo@gmail.com>
2025-03-12 16:50:14 +0800

    Add user repo

---
+++ app/models/repo/UserRepo.scala
@@ -0,0 +1,43 @@
+package models
+package repo
+
+import java.util.UUID
+import javax.inject.*
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.*
+import scala.annotation.targetName
+
+import play.api.db.slick.*
+
+import slick.jdbc.JdbcProfile
+
+import domain.*
+
+@Singleton
+class UserRepo @Inject() (
+    val dbConfigProvider: DatabaseConfigProvider
+) extends HasDatabaseConfigProvider[JdbcProfile] {
+  import dbConfig.profile.api.*
+
+  class UserTable(tag: Tag) extends Table[User](tag, "USER") {
+    val id       = column[UUID]("ID", O.PrimaryKey)
+    val name     = column[String]("NAME")
+    val email    = column[String]("EMAIL")
+    val username = column[String]("USERNAME")
+    val password = column[String]("PASSWORD")
+    val avatar   = column[Option[String]]("PASSWORD")
+
+    def * = (id, name, email, username, password, avatar).mapTo[User]
+  }
+
+  object Table extends TableQuery(new UserTable(_)) {
+    def create(user: User): Future[Int] = {
+      db.run(this += user)
+    }
+
+    def find(id: UUID): Future[Option[User]] = {
+      db.run(this.filter(_.id === id).result.headOption)
+    }
+  }
+}
