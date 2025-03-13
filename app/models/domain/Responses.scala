3089951a25811fe67dc3f97041e95f95cbcade1c
sirezo <joebertcerezo@gmail.com>
2025-03-13 11:37:35 +0800

    Add responses types

--- app/models/domain/Responses.scala
+++ app/models/domain/Responses.scala
@@ -18,4 +18,35 @@ private final case class Response(
       status(Json.obj("code" -> code, "data" -> data))
         .withSession(Session(data = session))
   }
+
+  def withSession(x: Map[String, String]): Response = copy(session = x)
+}
+
+opaque type ResponseSuccess = Response
+object ResponseSuccess {
+  def apply(
+      code: String,
+      status: Status,
+      data: JsValue = Json.obj(),
+      session: Map[String, String] = Map.empty
+  ): ResponseSuccess = Response(code, status, data, session)
+
+  extension (x: ResponseSuccess)
+    def toResult: Result = x.toResult
+    def withSession(session: (String, String)*): ResponseSuccess =
+      x.withSession(session.toMap)
+}
+
+opaque type ResponseError = Response
+object ResponseError {
+  def apply(
+      code: String,
+      status: Status,
+      data: JsValue = Json.obj(),
+      session: Map[String, String] = Map.empty
+  ): ResponseError = Response(code, status, data, session)
+  extension (x: ResponseError)
+    def toResult: Result = x.toResult
+    def withSession(session: (String, String)*): ResponseError =
+      x.withSession(session.toMap)
 }
