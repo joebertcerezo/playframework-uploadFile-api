36857f6e6e75fcbe397b8984942e4864f8b2f998
sirezo <joebertcerezo@gmail.com>
2025-04-08 13:36:23 +0800

    Create package.scala

---
+++ app/models/repo/package.scala
@@ -0,0 +1,15 @@
+package models
+
+import java.util.UUID
+
+import slick.jdbc.JdbcType
+import slick.jdbc.PostgresProfile.api.uuidColumnType
+import slick.jdbc.PostgresProfile.api.MappedColumnType
+
+import models.domain.types.*
+
+package object repo {
+
+  given JdbcType[IdUser] =
+    MappedColumnType.base[IdUser, UUID](_.value, IdUser.apply)
+}
