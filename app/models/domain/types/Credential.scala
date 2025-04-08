7c9292e09fc9d772899c8e9bf6d56026de7e82fd
sirezo <joebertcerezo@gmail.com>
2025-04-08 13:36:12 +0800

    Update Credential.scala

--- app/models/domain/types/Credential.scala
+++ app/models/domain/types/Credential.scala
@@ -3,31 +3,19 @@ package domain
 package types
 
 import play.api.libs.json.*
-import play.api.data.*
-import play.api.mvc.*
-import play.api.data.format.Formats.*
 import play.api.data.format.Formatter
-import play.api.*
 
 import JsonFormats.*
 
 opaque type Credential = String
 object Credential {
   def apply(value: String): Credential = value
+
   given Format[Credential] =
     opaqueFormat(_.validate[Credential], value => JsString(value.toString()))
 
+  given Formatter[Credential] = opaqueFormatter(Credential.apply, _.value)
   extension (c: Credential) {
     def value: String = c
-  }
-}
-
-object JsonFormats {
-  def opaqueFormat[T](
-      apply: JsValue => JsResult[T],
-      extract: T => JsValue
-  ): Format[T] = new {
-    def writes(t: T): JsValue             = extract(t)
-    def reads(json: JsValue): JsResult[T] = apply(json)
   }
 }
