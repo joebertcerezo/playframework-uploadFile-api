2239e117ea7f51bbc9208c9eee6f3f3c4d801bfd
sirezo <joebertcerezo@gmail.com>
2025-04-20 01:21:39 +0800

    Update FileController.scala

--- app/controllers/FileController.scala
+++ app/controllers/FileController.scala
@@ -3,8 +3,11 @@ package controllers
 import javax.inject.*
 
 import play.api.mvc.*
+import play.api.i18n.I18nSupport
 
 @Singleton
 class FileController @Inject() (
     cc: ControllerComponents
-) extends AbstractController(cc)
+) extends AbstractController(cc) with I18nSupport {
+    
+}
