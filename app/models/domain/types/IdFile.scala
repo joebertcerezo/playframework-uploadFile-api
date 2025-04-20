ee357edc821bf6e10b54c2a64a98305e18d7e146
sirezo <joebertcerezo@gmail.com>
2025-04-20 13:46:11 +0800

    Update IdFile.scala

--- app/models/domain/types/IdFile.scala
+++ app/models/domain/types/IdFile.scala
@@ -6,10 +6,21 @@ import java.util.UUID
 
 import play.api.libs.json.*
 import play.api.data.format.Formatter
+import play.api.data.format.Formats.uuidFormat
 
 import JsonFormats.*
 
 opaque type IdFile = UUID
 object IdFile {
   def apply(value: UUID): IdFile = value
+
+  given Format[IdFile] =
+    opaqueFormat(_.validate[UUID], value => JsString(value.toString()))
+
+  given Formatter[IdFile] =
+    opaqueFormatter(IdFile.apply, _.value)(using uuidFormat)
+
+  extension (x: IdFile) {
+    def value: UUID = x
+  }
 }
