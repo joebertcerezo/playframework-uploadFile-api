8956cfba0ef6c842e750be52bec12767db5f1a51
sirezo <joebertcerezo@gmail.com>
2025-04-20 22:30:54 +0800

    Update FileCreate.scala

--- app/models/domain/FileCreate.scala
+++ app/models/domain/FileCreate.scala
@@ -1,18 +1,25 @@
 package models
 package domain
 
+import java.util.UUID
+
 import play.api.libs.json.*
 
 import models.domain.types.*
 
-case class FileCreate(name: String, path: String, idUser: IdUser) {
-  def toDomain: File = File(name, path, idUser)
+case class FileCreate(
+    name: String,
+    path: Option[String]
+) {
+  def toDomain(path: String): File =
+    File(name, path)
 }
 
 object FileCreate {
   given Format[FileCreate] = Json.format[FileCreate]
 
-  def unapply(f: FileCreate): Option[(String, String, IdUser)] = Some(
-    Tuple.fromProductTyped(f)
-  )
+  def unapply(f: FileCreate): Option[(String, Option[String])] =
+    Some(
+      Tuple.fromProductTyped(f)
+    )
 }
