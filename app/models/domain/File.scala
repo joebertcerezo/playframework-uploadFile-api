ed57a792b2f4fc3e0f46ec2d41ecef6637e9c2f0
sirezo <joebertcerezo@gmail.com>
2025-04-20 18:33:14 +0800

    Update File.scala

--- app/models/domain/File.scala
+++ app/models/domain/File.scala
@@ -5,4 +5,13 @@ import java.util.UUID
 
 import types.*
 
-case class File(id: UUID, name: String, path: String, idUser: IdUser)
+case class File(id: IdFile, name: String, path: String, idUser: IdUser)
+
+object File {
+  def apply(name: String, path: String, idUser: IdUser): File =
+    new File(IdFile(UUID.randomUUID()), name, path, idUser)
+  
+  def unapply(
+      f: File
+  ): Option[(IdFile, String, String, IdUser)] = Some(Tuple.fromProductTyped(f))
+}
