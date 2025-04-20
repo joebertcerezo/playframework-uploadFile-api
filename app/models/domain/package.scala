85d6e4f523e0e5e6d70ada4b93a8442d142f9441
sirezo <joebertcerezo@gmail.com>
2025-04-20 13:46:06 +0800

    Update package.scala

--- app/models/domain/package.scala
+++ app/models/domain/package.scala
@@ -10,28 +10,38 @@ package object domain {
     def opaqueFormat[T](
         apply: JsValue => JsResult[T],
         extract: T => JsValue
-    ): Format[T] = new {
+    ): Format[T] = new Format[T] {
       def writes(t: T): JsValue             = extract(t)
       def reads(json: JsValue): JsResult[T] = apply(json)
     }
 
-    def opaqueFormatter[T](
-        apply: String => T,
-        extract: T => String
-    ): Formatter[T] =
+    /* def opaqueFormat[T](
+        apply: JsValue => JsResult[T],
+        extract: T => JsValue
+    ): Format[T] = new {
+      def writes(t: T): JsValue             = extract(t)
+      def reads(json: JsValue): JsResult[T] = apply(json)
+    } */
+
+    def opaqueFormatter[A, T](
+        apply: A => T,
+        extract: T => A
+    )(using formatter: Formatter[A]): Formatter[T] =
       new Formatter[T] {
         override def bind(
             key: String,
             data: Map[String, String]
         ): Either[Seq[FormError], T] =
-          data
-            .get(key)
-            .filter(_.nonEmpty)
-            .toRight(Seq(FormError(key, "error.required")))
-            .map(apply)
+          formatter.bind(key, data) match {
+            case Left(errors) => Left(errors)
+            case Right(value) => Right(apply(value))
+          }
 
         override def unbind(key: String, value: T): Map[String, String] =
-          Map(key -> extract(value))
+          formatter.unbind(
+            key,
+            extract(value)
+          ) 
       }
   }
 
