1139b330d953e22b3897ee9f7a6c057ded382cbe
sirezo <joebertcerezo@gmail.com>
2025-04-20 22:30:57 +0800

    Update ResponsesFile.scala

--- app/models/domain/ResponsesFile.scala
+++ app/models/domain/ResponsesFile.scala
@@ -4,8 +4,22 @@ package domain
 import play.api.libs.json.*
 import play.api.mvc.Results.*
 
-def FileCreated = ResponseSuccess(
+def FileCreated(data: JsValue) = ResponseSuccess(
   "FILE_CREATED",
   Created
 )
 
+def FileMoveFailed = ResponseError(
+  "FILE_MOVE_FAILED",
+  InternalServerError
+)
+
+def InvalidFile = ResponseError(
+  "INVALID_FILE",
+  BadRequest
+)
+
+def DirCreationFailed = ResponseError(
+  "DIR_CREATION_FAILED",
+  InternalServerError
+)
