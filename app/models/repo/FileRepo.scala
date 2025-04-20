8648bc0b9f50fa35586ba5a7c22417761048e3af
sirezo <joebertcerezo@gmail.com>
2025-04-20 18:33:20 +0800

    Create FileRepo.scala

---
+++ app/models/repo/FileRepo.scala
@@ -0,0 +1,36 @@
+package models
+package repo
+
+import javax.inject.*
+
+import play.api.db.slick.*
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+import slick.jdbc.JdbcProfile
+
+import domain.*
+import domain.types.*
+
+@Singleton
+class FileRepo @Inject() (
+    val dbConfigProvider: DatabaseConfigProvider
+) extends HasDatabaseConfigProvider[JdbcProfile] {
+  import dbConfig.profile.api.*
+
+  class FileTable(tag: Tag) extends Table[File](tag, "FILES") {
+    val id   = column[IdFile]("ID", O.PrimaryKey)
+    val name = column[String]("NAME")
+    val path = column[String]("PATH")
+    val idUser = column[IdUser]("IDUSER")
+
+    def * = (id, name, path, idUser).mapTo[File]
+  }
+
+  object Table extends TableQuery(new FileTable(_)){
+    def create(file: File): Future[Int] = {
+      db.run(this += file)
+    }
+  }
+}
