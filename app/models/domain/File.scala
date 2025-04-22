211d8484db09d8e6c1b2eba0f2fc3aa8433e70b5
sirezo <joebertcerezo@gmail.com>
2025-04-22 16:42:54 +0800

    Update File.scala

--- app/models/domain/File.scala
+++ app/models/domain/File.scala
@@ -3,11 +3,21 @@ package domain
 
 import java.util.UUID
 
+import play.api.libs.json.Writes
+import play.api.libs.json.Json
+import play.api.libs.json.JsValue
+
 import types.*
 
 case class File(id: IdFile, name: String, path: String, idUser: Option[IdUser])
 
 object File {
+  given Writes[File] = (o: File) =>
+    Json.obj(
+      "name" -> o.name,
+      "path" -> o.path
+    )
+
   def apply(name: String, path: String): File =
     new File(IdFile(UUID.randomUUID()), name, path, None)
 
