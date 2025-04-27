0a38cabab0263ed4bcb14ccf6eb3e64f4adf1100
sirezo <joebertcerezo@gmail.com>
2025-04-27 16:48:18 +0800

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,11 @@
 object Dependencies {
-  val pekko      = "1.2.0-M1"
-  val playMailer = "11.0.0-M1"
-  val slick      = "6.1.1"  // "6.1.1"
-  val postgres   = "42.7.5" // "42.7.4"
-  val catsCore   = "2.12.0"
-  val jbcrypt    = "0.4"
-  val pdfBox     = "3.0.4"
+  val pekko               = "1.2.0-M1"
+  val playMailer          = "11.0.0-M1"
+  val slick               = "6.1.1"  // "6.1.1"
+  val postgres            = "42.7.5" // "42.7.4"
+  val catsCore            = "2.12.0"
+  val jbcrypt             = "0.4"
+  val pdfBox              = "3.0.4"
+  val bouncyCastle_bcprov = "1.80"
+  val bouncyCastle_bcpkix = "1.80"
 }
